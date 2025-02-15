56ba9c209b9bccd0703ea9d99a78abda9f1fdfc6
andersonconsultant <andersonsilvads26@gmail.com>
2025-02-15 08:49:27 -0300

    h1 static_defined

--- api/cobol/index.js
+++ api/cobol/index.js
@@ -4,6 +4,14 @@ const { exec } = require('child_process');
 const iconv = require('iconv-lite');
 const app = express();
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../../')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../index.html'));
+});
+
 app.get('/api/cobol', (req, res) => {
   exec(__dirname + '/start', (error, stdout, stderr) => {
     if (error) {
@@ -22,6 +30,7 @@ app.get('/api/cobol', (req, res) => {
     res.send({ cobolOutput: iconv.decode(stdout, 'latin1') });
   });
 });
-app.listen(3000, () => {
-  console.log('API rodando na porta 3000');
+app.listen(3000, '0.0.0.0', () => {
+  console.log('API rodando na porta 3000 e acessível em http://192.168.0.15:3000');
 });
+
