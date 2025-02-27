2e57ad07d1b9fa7b1304db12e8f47e83dcf63dfc
andersonconsultant <andersonsilvads26@gmail.com>
2025-02-27 09:51:12 -0300

    h4 upgrade css style and folders

--- api/index.js
+++ api/index.js
@@ -1,36 +1,37 @@
 const express = require('express');
+const https = require('https');  // Adicionado para HTTPS
 const fs = require('fs');
 const { exec } = require('child_process');
 const iconv = require('iconv-lite');
-const app = express();
-
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../../')));
+const app = express();
 
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Rota principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.get('/api/cobol', (req, res) => {
-  exec(__dirname + '/start', (error, stdout, stderr) => {
+// Rota para executar o programa COBOL
+app.get('/cobol', (req, res) => {
+  exec(path.join(__dirname, 'start'), (error, stdout, stderr) => {
     if (error) {
       console.error(`Erro: ${error.message}`);
       res.status(500).send({ error: 'Erro ao executar o programa COBOL' });
-
       return;
     }
-
     if (stderr) {
       console.error(`Erro: ${stderr}`);
       res.status(500).send({ error: 'Erro ao executar o programa COBOL' });
       return;
     }
-
     res.send({ cobolOutput: iconv.decode(stdout, 'latin1') });
   });
 });
-app.listen(3000, '0.0.0.0', () => {
-  console.log('API rodando na porta 3000 e acessível em http://192.168.0.15:3000');
-});
 
+app.listen(3000, '0.0.0.0', () => {
+  console.log('API rodando na porta 3000 em http://localhost:3000');
+});
